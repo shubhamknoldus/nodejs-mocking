54c5dee8eb8135e14dd32f0aa907597fca6506f3
abaranwal <abhishek.baranwal@knoldus.in>
2019-04-14 16:51:14 +0530

    Added testcase demonstrating monkey-patching

---
+++ README.md
@@ -0,0 +1,1 @@
+clone the repo and just run *npm install* to install the node dependencies and then run _npm test_
---
+++ blog/dependency.js
@@ -0,0 +1,12 @@
+const add = (x, y) => x + y;
+const sub = (x, y) => x >= y ? x - y : y - x;
+const callBackDivide = (x, y, callback) => {
+    y === 0 ? callback("Can't divide with zero", null) :
+        callback(null, x / y)
+};
+
+module.exports = {
+    add,
+    sub,
+    callBackDivide
+}
